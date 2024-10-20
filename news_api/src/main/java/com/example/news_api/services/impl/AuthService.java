bb7e89e328fa345750a6424a2e24caaa3de4b383
Radenko Setka <Radenko.Setka@prointer.ba>
2024-10-21 00:11:52 +0200

    Removed imports vol2

--- news_api/src/main/java/com/example/news_api/services/impl/AuthService.java
+++ news_api/src/main/java/com/example/news_api/services/impl/AuthService.java
@@ -8,8 +8,6 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-
 @Service
 @Transactional
 @RequiredArgsConstructor
